cc94289959b34fb9851fcea8db9751d975d00bef
joyant-s-g-j <joyantsheikharguptajoy@gmail.com>
2026-03-11 19:40:49 +0600

    padding nav

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -56,7 +56,7 @@ export function Navbar({ categories: initialCategories = [] }: { settings?: Reco
 
   return (
     <div
-      className={`flex justify-between items-center fixed top-0 left-0 right-0 py-3 px-4 sm:px-6 lg:px-10 z-50 transition-all duration-300 overflow-x-hidden ${isScrolled ? " backdrop-blur-md" : "bg-transparent"}`}
+      className={`flex justify-between items-center fixed top-0 left-0 right-0 py-3 px-4 sm:px-6 lg:px-24 z-50 transition-all duration-300 overflow-x-hidden ${isScrolled ? " backdrop-blur-md" : "bg-transparent"}`}
     >
       <div className="flex items-center gap-2">
         <Link href="/">
